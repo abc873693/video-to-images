9326c0db103e4aa75204319e3e52add83615ec3b
Rainvisitor <abc873693@gmail.com>
2019-09-23 15:00:57 +0800

    Update function name cv2 to cv

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 
-import cv2
+from cv2 import cv2 as cv
 import os
 import argparse
 
 out_path = './out'
 
-if __name__=='__main__':
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '-i', '--input_path',
@@ -25,9 +25,9 @@ if __name__=='__main__':
     size = args.resize
 
     if not os.path.exists(out_path):
-            os.makedirs(out_path)
+        os.makedirs(out_path)
     # '/Users/rainvisitor/Movies/黃英哲老師實驗室提供影片/可見光/濁.avi'
-    vc = cv2.VideoCapture(source_path)
+    vc = cv.VideoCapture(source_path)
     index = 0
     rval = vc.isOpened()
     while rval:
@@ -35,9 +35,11 @@ if __name__=='__main__':
         rval, frame = vc.read()
         if rval:
             if(size != 0):
-                frame = cv2.resize(frame, (size, size), interpolation=cv2.INTER_CUBIC)
-            cv2.imwrite('./out/frame'+str(index).zfill(5) + '.jpg', frame)
-            cv2.waitKey(1)
+                frame = cv.resize(frame, (size, size),
+                                  interpolation=cv.INTER_CUBIC)
+            #frame = cv.rotate(frame, cv.ROTATE_90_COUNTERCLOCKWISE)
+            cv.imwrite('./out/frame'+str(index).zfill(5) + '.jpg', frame)
+            cv.waitKey(1)
         else:
             break
     vc.release()
